f330ef877cfc21c30724b8b2a02d1f60117df894
Jackono Fabella <jackonofabella@gmail.com>
2024-03-29 03:20:39 +1100

    middleman v0.0.2

--- middleman/deploy.sh
+++ middleman/deploy.sh
@@ -1,8 +1,8 @@
 # Build the docker image
-docker build -t us-central1-docker.pkg.dev/nextgem-selenium/request-queue/middleman:v0.0.1 .
+docker build -t us-central1-docker.pkg.dev/nextgem-selenium/request-queue/middleman:v0.0.2 .
 
 # Push the docker image to the Google Cloud Registry
-docker push us-central1-docker.pkg.dev/nextgem-selenium/request-queue/middleman:v0.0.1
+docker push us-central1-docker.pkg.dev/nextgem-selenium/request-queue/middleman:v0.0.2
 
 # Deploy the worker to Cloud Run
 kubectl apply -f ./kubernetes/deployment.yaml
