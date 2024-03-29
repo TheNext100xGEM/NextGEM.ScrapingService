33de88e52043403243571c580908ee371357e946
Jackono Fabella <jackonofabella@gmail.com>
2024-03-29 20:35:51 +1100

    add type checking

--- middleman/src/index.ts
+++ middleman/src/index.ts
@@ -1,111 +1,22 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
-import * as crypto from 'crypto';
 import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 
-const API_BASE_URL = process.env.API_BASE_URL;
-const LIMIT_REQUESTS = 2;
-const domainQueues: {
-  [domainName: string]: {
-    activeRequests: number;
-    queue: { url: string; res: Response; endpoint: string }[];
-  };
-} = {};
-const getDomainName = (url: string): string | null => {
-  try {
-    return new URL(url).hostname;
-  } catch (error) {
-    return null;
-  }
-};
-
-interface PendingRequest {
-  url: string;
-  isFinished: boolean;
-  html: string;
-  timeout?: NodeJS.Timeout;
+interface ScrapeData {
+  text: string;
 }
 
-const pendingRequests: { [requestId: string]: PendingRequest } = {};
+interface ScrapeResult {
+  url: string;
+  data?: ScrapeData[];
+  error?: string;
+}
 
-const randomHex = (length: number): string => {
-  const numberOfBytes = Math.ceil(length / 2);
-  const randomBytes = crypto.randomBytes(numberOfBytes);
-  return randomBytes.toString('hex').slice(0, length);
-};
-
-const enqueueRequest = async (domainName: string, url: string, res: Response, endpoint: string): Promise<string> => {
-  const requestId = randomHex(48);
-  if (!domainQueues[domainName]) {
-    domainQueues[domainName] = { activeRequests: 0, queue: [] };
-  }
-  pendingRequests[requestId] = { url, isFinished: false, html: '' };
-  domainQueues[domainName].queue.push({ url, res, endpoint });
-  const response = await processQueueForDomain(domainName, requestId);
-  return response;
-};
-
-const processQueueForDomain = async (domainName: string, requestId?: string): Promise<string> => {
-  const domainQueue = domainQueues[domainName];
-  if (!domainQueue) return;
-
-  if (domainQueue.activeRequests < LIMIT_REQUESTS && domainQueue.queue.length > 0) {
-    const shiftedElement = domainQueue.queue.shift();
-    if (shiftedElement) {
-      const { url, res, endpoint } = shiftedElement;
-      domainQueue.activeRequests++;
-
-      console.log(`Processing ${url} for ${endpoint} in domain: ${domainName}`);
-
-      let apiEndpoint: string;
-
-      switch (endpoint) {
-      case 'scrape':
-        apiEndpoint = `${process.env.API_BASE_URL}/scrape`;
-        break;
-      case 'scrape_soup':
-        apiEndpoint = `${process.env.API_BASE_URL}/scrape_soup`;
-        break;
-      default:
-        throw new Error('Invalid endpoint');
-      }
-
-      try {
-        // res.send({ requestId });
-        const response = await axios.post(apiEndpoint, { url });
-        // console.log(response.data);
-        pendingRequests[requestId!].isFinished = true;
-        pendingRequests[requestId!].html = response.data?.html;
-
-        pendingRequests[requestId!].timeout = setTimeout(() => {
-          delete pendingRequests[requestId!];
-        }, 60 * 1000 * 2);
-        return response.data.text;
-      } catch (error) {
-        res.status(500).send('Error processing your request');
-      }
-      domainQueue.activeRequests--;
-      processQueueForDomain(domainName);
-    }
-    return;
-  }
-};
-
-app.get('/result/:requestId', (req: Request, res: Response) => {
-  const { requestId } = req.params;
-  const request = pendingRequests[requestId];
-
-  if (request) {
-    const { isFinished, html, url } = request;
-    res.json({ isFinished, html, url });
-  } else {
-    res.status(404).send('Request not found');
-  }
-});
+const API_BASE_URL = process.env.API_BASE_URL;
 
 app.post('/scrape', async (req: Request, res: Response) => {
   try {
@@ -119,7 +30,7 @@ app.post('/scrape', async (req: Request, res: Response) => {
     // Send requests to scrape each URL concurrently
     await Promise.all(urls.map(async (url) => {
       try {
-        const response = await axios.post(`${process.env.API_BASE_URL}/scrape`, { url });
+        const response = await axios.post(`${API_BASE_URL}/scrape`, { url });
         results.push({ url, data: response.data });
       } catch (error) {
         results.push({ url, error: error.message });
@@ -140,11 +51,11 @@ app.post('/scrape_soup', async (req: Request, res: Response) => {
       return res.status(400).json({ error: 'Invalid URLs array' });
     }
 
-    const results: any[] = [];
+    const results: ScrapeResult[] = [];
     // Send requests to scrape each URL concurrently
     await Promise.all(urls.map(async (url) => {
       try {
-        const response = await axios.post(`${process.env.API_BASE_URL}/scrape_soup`, { url });
+        const response = await axios.post(`${API_BASE_URL}/scrape_soup`, { url });
         results.push({ url, data: response.data });
       } catch (error) {
         results.push({ url, error: error.message });
